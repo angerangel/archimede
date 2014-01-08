dec9fd3377bb414c14a34a98b19badf3e59f745d
angerangel@gmail.com <angerangel@gmail.com>
2014-01-08 18:02:40 +0100

    Aggiunto cartella di sicurezza

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <? php
 //questa deve essere la pagina di avvio
-//Si chiede il nome e lo si memorizza in un cookie
+//Tutti i valori duraturi nel temp saranno memorizzati in una sessione
 ?>
 <div align=center >
 <h1>ARCHIMEDE</h1>
 
 Benvenuto su Archimede, il gioco di intelligenza che ti mettera' a dura prova. Riuscirai a superare tutte e 15 le domande?<br>
 Per prima cosa ci serve sapere il tuo nome:
-<form>
+<form action=nome.php method=post >
+<input type=text >
+<br>
+<input type=submit name=submit value="Comincia!" >
 </form>
-
-
-
-
 
 </div>
