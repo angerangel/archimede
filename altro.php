d24f8bfc46d95e80b6c49ebc96196bda0b6eaf72
angerangel@gmail.com <angerangel@gmail.com>
2014-01-23 13:48:36 +0100

    Prima modifica per mettere gli amministratori

--- altro.php
+++ altro.php
@@ -1,3 +1,28 @@
 <?php
 #put your custom contente here
+require_once 'detectmobile.php' ;
+if ($mobile) {
+	echo "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
+		<!-- archimede3 -->
+		<ins class=\"adsbygoogle\"
+		style=\"display:inline-block;width:320px;height:50px\"
+		data-ad-client=\"ca-pub-9291179133147047\"
+		data-ad-slot=\"8965379764\"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	";
+	} else {
+	echo "<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
+		<!-- archimede -->
+		<ins class=\"adsbygoogle\"
+		style=\"display:inline-block;width:728px;height:90px\"
+		data-ad-client=\"ca-pub-9291179133147047\"
+		data-ad-slot=\"7727808963\"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>";
+	}
 ?>
+
+
---
+++ secret/admins.php
@@ -0,0 +1,4 @@
+<?php
+#Inserire qui nomi e password degli amministratori tipo il seguente
+$admins["user"] = "password";
+?>
